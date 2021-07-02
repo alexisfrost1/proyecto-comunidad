d4e0081f8a456c21142cf4ad76665e586f8e9b05
alexisfrost1 <70116175+alexisfrost1@users.noreply.github.com>
2021-07-02 14:34:43 -0400

    Add files via upload

---
+++ src/app/Pages/mantenciones/mantenciones.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MantencionesComponent } from './mantenciones.component';
+
+describe('MantencionesComponent', () => {
+  let component: MantencionesComponent;
+  let fixture: ComponentFixture<MantencionesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MantencionesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MantencionesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { EncomiendasComponent } from './Pages/encomiendas/encomiendas.component';
+import { GastosComunesComponent } from './Pages/gastos-comunes/gastos-comunes.component';
+import { MantencionesComponent } from './Pages/mantenciones/mantenciones.component';
+import { ReservasComponent } from './Pages/reservas/reservas.component';
+import { VisitasComponent } from './Pages/visitas/visitas.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, children:[
+    {path: 'nav', component: MainNavComponent},
+    {path: 'encomiendas', component: EncomiendasComponent},
+    {path: 'mantenciones',component: MantencionesComponent},
+    {path: 'reservas',component: ReservasComponent},
+    {path: 'visitas', component: VisitasComponent},
+    {path:'inicio', component:InicioComponent},
+    {path: 'gastos-comunes', component: GastosComunesComponent},
+  ]},
+  {path: 'login', component: LoginComponent},
+  {path: '' , component:LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
