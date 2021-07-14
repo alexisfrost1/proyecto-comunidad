1076e1cbb82483ac2ee520ff82d1a9c33e8591a0
firedfalcon <firedfalcon@gmail.com>
2021-07-14 15:04:51 -0400

    Add files via upload

--- src/app/Pages/reservas/reservas.component.ts
+++ src/app/Pages/reservas/reservas.component.ts
@@ -1,3 +1,40 @@
+<<<<<<< HEAD
+import { Component, OnInit } from '@angular/core';
+
+
+interface area {
+    value: number;
+    viewvalue: string;
+}
+
+@Component({
+    selector: 'app-reservas',
+    templateUrl: './reservas.component.html',
+    styleUrls: ['./reservas.component.css']
+})
+
+export class ReservasComponent implements OnInit {
+
+    areaComun: area[] = [{ value: 1, viewvalue: 'Piscina Block n°3' },
+                         { value: 2, viewvalue: 'Quincho Terraza Block n°2' },
+                         { value: 3, viewvalue: 'Multicancha Plaza n°2' }];
+    areaReserva: String = ''; 
+
+    minDate: Date;
+    maxDate: Date;
+    fechaReserva: Date = new Date();
+
+    constructor() {
+        const currentDate = new Date();
+        this.minDate = new Date();
+        this.maxDate = new Date(currentDate.setDate(currentDate.getDate() + 90));
+    } 
+
+  ngOnInit(): void {
+  }
+
+}
+=======
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,18 +44,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservasComponent implements OnInit {
 
-    minDate: Date;
-    maxDate: Date;
-
-    constructor() {
-        const currentDate = new Date();
-        this.minDate = new Date();
-        this.maxDate = new Date(currentDate.setDate(currentDate.getDate() + 90));
-    }
-
-    
+  constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+>>>>>>> parent of 2201d47 (Add files via upload)
