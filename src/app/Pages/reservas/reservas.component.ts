705b1aeb1e9c669f3c3318168bfac2b55ec26158
Fabian Contreras Osorio <firedfalcon@gmail.com>
2021-07-14 16:20:19 -0400

    Add files via upload

--- src/app/Pages/reservas/reservas.component.ts
+++ src/app/Pages/reservas/reservas.component.ts
@@ -33,19 +33,3 @@ export class ReservasComponent implements OnInit {
   }
 
 }
-=======
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-reservas',
-  templateUrl: './reservas.component.html',
-  styleUrls: ['./reservas.component.css']
-})
-export class ReservasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
