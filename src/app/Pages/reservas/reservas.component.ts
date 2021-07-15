296aee2eb193d224a607d225244bac9c166f6f2f
Fabian Contreras Osorio <firedfalcon@gmail.com>
2021-07-15 16:14:46 -0400

    Reservas update

--- src/app/Pages/reservas/reservas.component.ts
+++ src/app/Pages/reservas/reservas.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 interface area {
-    value: number;
-    viewvalue: string;
+    n_area: number;
+    nombre_area: string;
+}
+
+interface reserva {
+    nombre: string;
+    rut: string;
+    fecha: Date;
+    n_area: number;
+    nombre_area: string;
+}
+
+interface reserva_comunidad {
+    n_area: number;
+    fecha: Date;
 }
 
 @Component({
@@ -13,19 +26,39 @@ interface area {
 
 export class ReservasComponent implements OnInit {
 
-    areaComun: area[] = [{ value: 1, viewvalue: 'Piscina Block n°3' },
-                         { value: 2, viewvalue: 'Quincho Terraza Block n°2' },
-                         { value: 3, viewvalue: 'Multicancha Plaza n°2' }];
-    areaReserva: String = ''; 
-
     minDate: Date;
     maxDate: Date;
     fechaReserva: Date = new Date();
+    currentDate = new Date();
+
+    areaComun: area[] = [{ n_area: 1, nombre_area: 'Piscina Block n°3' },
+                         { n_area: 2, nombre_area: 'Quincho Terraza Block n°2' },
+                         { n_area: 3, nombre_area: 'Cancha de futbol Plaza n°2' }];
+    nReserva: number = 0; 
+
+    reservasUnidad: reserva[] = [{ nombre: 'Alexis Canessa', rut: '11111', fecha: this.currentDate, n_area: 1, nombre_area: 'Piscina Block n°3' }];
+
+    reservasComunidad: reserva_comunidad[] = [{ n_area: 1, fecha: this.currentDate },
+        { n_area: 1, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 5)) },
+        { n_area: 1, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 7)) },
+        { n_area: 1, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 14)) },
+        { n_area: 2, fecha: this.currentDate },
+        { n_area: 2, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 6)) },
+        { n_area: 2, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 9)) },
+        { n_area: 2, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 20)) },
+        { n_area: 3, fecha: this.currentDate },
+        { n_area: 3, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 4)) },
+        { n_area: 3, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 10)) },
+        { n_area: 3, fecha: new Date(this.currentDate.setDate(this.currentDate.getDate() + 17)) }];
 
     constructor() {
-        const currentDate = new Date();
+
+        //* Rango de fechas en las que es posible reservar*//
+
         this.minDate = new Date();
-        this.maxDate = new Date(currentDate.setDate(currentDate.getDate() + 90));
+        this.maxDate = new Date(this.currentDate.setDate(this.currentDate.getDate() + 90));
+
+
     } 
 
   ngOnInit(): void {
