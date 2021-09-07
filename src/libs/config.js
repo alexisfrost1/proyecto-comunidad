88bb6be0d38245fe7d85f0644efc2e17d640a342
firedfalcon <firedfalcon@gmail.com>
2021-09-07 17:40:21 -0300

    Delete config.js

--- src/libs/config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-	HOST		= process.env.HOST || '138.121.171.19',
-	USER		= process.env.USER || 'falcon',
-	PASSWORD	= process.env.PASSWORD || 'Soporte2021',
-	DATABASE	= process.env.DATABASE || 'CommunityAppDB',
-	PORT		= process.env.PORT || 1433,
-	INSTANCENAME= process.env.INSTANCENAME || 'SQLEXPRESS'
-}
